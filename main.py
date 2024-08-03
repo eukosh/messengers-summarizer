c9624a73cb2839adf48c37d2b454c4e111202893
Eugene Kosharnyi <eugene.kosharnyi@kununu.com>
2024-08-03 16:18:29 +0200

    Finished telegram provider implementation.

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ if __name__ == "__main__":
     # Usage example
     messenger = MessengerFactory.get_or_create_messenger("telegram")()
     print(messenger)
-    messenger.read_messages()
+    messages = messenger.read_messages()
+    print(messages)
--- src/messengers/providers/telegram.py
+++ src/messengers/providers/telegram.py
@@ -5,8 +5,9 @@ from typing import List, Optional, Union
 from telethon import TelegramClient
 from telethon.tl.types import Message
 
+from src.const import CHAT_ID_MAPPING
 from src.messengers.base_messenger import BaseMessenger
-from src.utils import extract_props_from_obj_to_dict, print_message
+from src.utils import extract_props_from_obj_to_dict
 
 TG_APP_ID = os.environ["TG_APP_ID"]
 TG_API_HASH = os.environ["TG_API_HASH"]
@@ -24,13 +25,6 @@ class TelegramMessenger(BaseMessenger):
                 return client.loop.run_until_complete(func(self, *args, **kwargs))
 
         return wrapper
-
-    # @staticmethod
-    # def print_dialog_id(channel_name: str, dialog):
-    #     channel_name = channel_name.lower()
-    #     dialog_name = dialog.name.lower()
-    #     if channel_name in dialog_name:
-    #         print(dialog.name, "has ID", dialog.id)
 
     @run_with_client
     async def print_dialog_ids(self, channel_name: Union[str, List[str]]):
@@ -59,9 +53,12 @@ class TelegramMessenger(BaseMessenger):
         channel_id: int,
         start_date: Optional[datetime.datetime] = None,
         extract_props: Optional[List[str]] = None,
+        skip_empty_messages: bool = True,
     ) -> List[Message]:
         messages = []
         async for message in self.client.iter_messages(channel_id, offset_date=start_date, reverse=True):
+            if skip_empty_messages and not message.message:
+                continue
             if extract_props:
                 messages.append(extract_props_from_obj_to_dict(message, extract_props))
             else:
@@ -69,16 +66,21 @@ class TelegramMessenger(BaseMessenger):
 
         return messages
 
-    def read_messages(self, start_date: Optional[datetime.datetime] = None):
+    def read_messages(self, start_date: Optional[datetime.datetime] = None, skip_empty_messages: bool = True):
         if not start_date:
             start_date = datetime.datetime.now() - datetime.timedelta(hours=10)
         print(f"Reading with offset_date: {start_date}")
 
         # self.print_dialog_ids(["Арина", "Українці в Австрії", "Австрия Бизнес", "Австрія IT"])
-        messages = self.get_messages(
-            -1001718919267, start_date, extract_props=["id", "chat_id", "sender_id", "date", "message"]
-        )
-        # messages = self.get_messages(-1001718919267, start_date)
 
-        for message in messages:
-            print_message(message)
+        messages = {}
+        for channel_name, channel_id in CHAT_ID_MAPPING.items():
+            chat_messages = self.get_messages(
+                channel_id,
+                start_date,
+                extract_props=["id", "chat_id", "sender_id", "date", "message"],
+                skip_empty_messages=skip_empty_messages,
+            )
+            messages[channel_name] = chat_messages
+
+        return messages
