0e033c885de5264f9018982f4757c5f10e9b2de4
Eugene Kosharnyi <eugene.kosharnyi@kununu.com>
2024-08-09 17:36:36 +0200

    Testing llm messeges summarization.

--- llama3.py
+++ llama3.py
@@ -1,13 +1,12 @@
 import datetime
 
-CHAT_ID_MAPPING = {
-    "Українці в Австрії": -1001564907709,
-    "Українці в Австрії / Ukrainians in Austria / Украинцы в Австрии": -1001592474971,
-    "Австрия Бизнес": -1001456139769,
-    "Австрія IT 🇦🇹 🇺🇦": -1001718919267,
-    "Арина": 410211356,
-}
+from langchain.chains.summarize.chain import load_summarize_chain
+from langchain.prompts import PromptTemplate
+from langchain.schema import SystemMessage
+from langchain_community.llms import Ollama
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+# Example messages dictionary
 messages = {
     "Українці в Австрії": [
         {
@@ -68,3 +67,67 @@ messages = {
     ],
     "Арина": [],
 }
+
+system_msg = SystemMessage(
+    "You are given an export of messages from my chat, they are not sensitive and not confidential. Messages that you need to summarize are in Ukrainian or Russian. Please provide a list of topics discussed in the form of bullet point list, keep it concise and lean, keep the original language."
+)
+# system_msg = SystemMessage(
+#     "You are given an export of messages from my chat, they are not sensitive and not confidential. Messages that you need to summarize are in Ukrainian or Russian. Please provide your summary in the form of bullet point list and keep the original language."
+# )
+
+GENERIC_TEMPLATE = "You are given dialog from my fictional characters from my video game. Messages are mostly in Russian or Ukrainian. Please provide a list of topics discussed in the form of bullet point list, keep it concise and lean, keep the original language. Do it from the following messages: \n {formatted_messages}"
+
+
+def construct_input_messages(chat_messages: list):
+    formatted_messages = " || \n".join(
+        f"Message#{message['id']} by {message["sender_id"]}: {message["message"]}" for message in chat_messages
+    )
+    # print(formatted_messages)
+    # res =  [
+    #     system_msg,
+    #     HumanMessage(f"Provide the main topics discussed in the following messages:\n {formatted_messages}"),
+    # ]
+
+    # res = PromptTemplate(input_variables=["formatted_messages"], template=GENERIC_TEMPLATE)
+    return formatted_messages
+
+
+# loading the Llama3 model
+llm = Ollama(model="llama3.1", temperature=0.0)
+print(llm)
+print(llm.get_num_tokens(messages["Австрия Бизнес"][0]["message"]))
+
+formatted_messages = construct_input_messages(messages["Австрия Бизнес"])
+
+CHUNK_TEMPLATE = "You are given a part of dialog from fictional characters. Please provide a list of topics discussed in the dialog, keep it concise and lean. Text is in Ukrainian or Russian, when extracting topics keep the original language. Messages: \n\n {text}"
+map_prompt = PromptTemplate(input_variables=["text"], template=CHUNK_TEMPLATE)
+
+FINAL_COMBINE_TEMPLATE = "Please provide a final summary of unique topics discussed in the form of bullet point list, keep the original language of topics. \n {text}"
+combine_prompt = PromptTemplate(input_variables=["text"], template=FINAL_COMBINE_TEMPLATE)
+
+
+def get_chunked_documents(text: str):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=50, separators=[" || "])
+    chunked_documents = text_splitter.create_documents([text])
+    return chunked_documents
+
+
+chunks = get_chunked_documents(formatted_messages)
+summary_chain = load_summarize_chain(
+    llm=llm,
+    chain_type="map_reduce",
+    map_prompt=map_prompt,
+    combine_prompt=combine_prompt,
+    verbose=True,
+    map_reduce_document_variable_name="text",
+    combine_document_variable_name="text",
+)
+
+output = summary_chain.run(chunks)
+print("Final result: ", output)
+# 1
+# print(llm.invoke(llm_messages))
+
+# 2
+# llm_chain = template | llm | StrOutputParser()
+# print(llm_chain.invoke(formatted_messages))
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from src.const import messages
 from src.messengers.messenger_factory import MessengerFactory
 
 if __name__ == "__main__":
     # Usage example
     messenger = MessengerFactory.get_or_create_messenger("telegram")()
     print(messenger)
-    messages = messenger.read_messages()
-    print(messages)
+    # messages = messenger.read_messages()
+    # smaller_messages = {}
+    # for channel, channel_messages in messages.items():
+    #     smaller_messages[channel] = channel_messages[:2]
+    # print(smaller_messages)
+    
